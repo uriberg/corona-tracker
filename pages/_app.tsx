fdd9ce435506eb28130bc747944dc2f9f94849fd
Uri_Berger <uribe1927@gmail.com>
2020-06-10 03:46:11 +0300

    adding fomantic-ui-css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import App, { Container } from 'next/app'
 import React from 'react'
 import Layout from "../components/Layout";
 import '../sass/main.scss';
+import 'fomantic-ui-css/semantic.css';
 
 export default class MyApp extends App {
     render () {
